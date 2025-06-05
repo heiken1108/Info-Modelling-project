b7e1a0431a2f9e9f147780690d9ad4d09e825f1b
Håkon Hoelsæter <hakon1108@gmail.com>
2025-06-05 13:09:51 +0200

    Added func to map

--- norwegian-cuisine/src/pages/MapPage.tsx
+++ norwegian-cuisine/src/pages/MapPage.tsx
@@ -1,35 +1,48 @@
+import { useNavigate } from 'react-router-dom';
 import '../styling/MapPage.css';
 
 interface Room {
 	roomName: string;
 	items: string[];
+	url: string;
 }
 
 function MapPage() {
+	const navigate = useNavigate();
+	const handleClick = (url: string) => {
+		navigate(`${url}`);
+	};
+
 	const rooms: Room[] = [
 		{
 			roomName: 'Hall of The Industrial Revolution',
 			items: ['Brunost', 'Ribbe', 'Vafler'],
+			url: '../narrative/67e539b42c27f289cb1a9cc0/chapter/3',
 		},
 		{
 			roomName: 'Hall of the Early Modern Period',
 			items: ['Pinnekjøtt', 'Smalahove', 'Finnbiff', 'Gløgg'],
+			url: '../narrative/67e539b42c27f289cb1a9cc0/chapter/2',
 		},
 		{
 			roomName: 'Hall of the Middle Ages',
 			items: ['Fårikål', 'Røkelaks'],
+			url: '../narrative/67e539b42c27f289cb1a9cc0/chapter/1',
 		},
 		{
 			roomName: 'Hall of the Modern Era',
 			items: ['Wienerpølse', 'Grandiosa', 'Taco'],
+			url: '../narrative/67e539b42c27f289cb1a9cc0/chapter/4',
 		},
 		{
 			roomName: 'Entrance',
 			items: [],
+			url: '../',
 		},
 		{
 			roomName: 'Hall of the Viking Age',
 			items: ['Tørrfisk', 'Rakfisk', 'Sursild'],
+			url: '../narrative/67e539b42c27f289cb1a9cc0/chapter/0',
 		},
 	];
 
@@ -38,8 +51,13 @@ function MapPage() {
 			<h1>Map of Nordic Bites</h1>
 			<div className="map-div">
 				<div className="museum-map">
-					{rooms.map(({ roomName, items }) => (
-						<div className="room" key={roomName}>
+					{rooms.map(({ roomName, items, url }) => (
+						<div
+							className="room"
+							key={roomName}
+							onClick={() => handleClick(url)}
+							style={{ cursor: 'pointer' }}
+						>
 							<h3>{roomName}</h3>
 							<ul>
 								{items.map((item) => (
