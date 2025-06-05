498d4ca8ac1a1bb44191f05d1ce00dfa9a869cc9
Håkon Hoelsæter <hakon1108@gmail.com>
2025-06-05 19:04:42 +0200

    More chabges

--- norwegian-cuisine/vite.config.ts
+++ norwegian-cuisine/vite.config.ts
@@ -4,11 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	resolve: {
-		alias: {
-			'@mui/styled-engine': '@mui/styled-engine-sc',
-		},
-	},
 	server: {
 		proxy: {
 			'/api': 'http://localhost:5001',
